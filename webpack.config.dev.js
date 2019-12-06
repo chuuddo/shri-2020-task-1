19d1bcfa4201540857658c2947485220f41260ff
Ilya Chudin <chudin.ilya@gmail.com>
2019-12-07 00:25:15 +0400

    fix js bundle filename
    split webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  entry: {
+    script: "./src/main.js",
+    index: "./src/pages/index.js",
+    indexOverlay: "./src/overlays/index.js",
+    product: "./src/pages/product.js",
+    productOverlay: "./src/overlays/product.js"
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 8192
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "index",
+      filename: "index.html",
+      chunks: ["script", "index", "indexOverlay"]
+    }),
+    new HtmlWebpackPlugin({
+      title: "product",
+      filename: "product.html",
+      chunks: ["script", "product", "productOverlay"]
+    })
+  ],
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000
+  }
+};
---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/main.js",
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "build")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg)$/i,
+        loader: "url-loader",
+        options: {
+          limit: 8192
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    })
+  ]
+};
