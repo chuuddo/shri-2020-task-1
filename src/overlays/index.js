4d1396caf4a6660b9c2485a4cf9277438192883c
Ilya Chudin <chudin.ilya@gmail.com>
2019-12-08 22:38:02 +0400

    add expanded history overlay
    fix overlays width

--- src/overlays/index.js
+++ src/overlays/index.js
@@ -2,5 +2,5 @@ import "./overlay.scss";
 
 document.body.insertAdjacentHTML(
   "afterbegin",
-  '<div class="overlay overlay__index"></div>'
+  '<div class="overlay overlay__index"></div><div class="overlay overlay__history2"></div>'
 );
