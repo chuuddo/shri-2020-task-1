a2a41e3b3348f82368d2017c3dfe38751315ac82
Ilya Chudin <chudin.ilya@gmail.com>
2019-12-09 13:56:03 +0400

    add DOMContentLoaded event handler

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import "./main.scss";
 import accordionHandler from "./common.blocks/e-accordion";
 import onoffswitchHandler from "./content.blocks/onoffswitch";
 
-document.body.addEventListener("click", accordionHandler);
-document.body.addEventListener("click", onoffswitchHandler);
+document.addEventListener("DOMContentLoaded", () => {
+  document.body.addEventListener("click", accordionHandler);
+  document.body.addEventListener("click", onoffswitchHandler);
+});
